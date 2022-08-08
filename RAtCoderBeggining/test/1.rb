ccafcc8088fc687a60b9e1f06540eb8a93d52684
ogakimamoru <ogaki.mamoru@gmail.com>
2022-08-08 12:45:14 +0900

    rubyでatcoderするときのサンプル

---
+++ RAtCoderBeggining/test/1.rb
@@ -0,0 +1,21 @@
+print "Please input your name: "
+STDOUT.flush
+
+# chompは文字列末尾の\nを取り除く
+name = gets.to_s.chomp
+
+# 入力した値を使用してメッセージを出力
+puts "Hello #{name}!"
+
+
+# 数値をコンソールから入力する
+print "Please input your age: "
+STDOUT.flush
+age = gets.to_i
+puts "You are #{age} years old."
+# Note: 数値以外の入力があると、ArgumentErrorが発生します。
+
+inputs = gets.split(' ')
+inputs.each do |value|
+  puts value
+end
